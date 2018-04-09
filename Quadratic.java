ed986f45622bd45fda831d0ac37f8cd3c564e929
jawauntb <Juan@Jawauns-MacBook-Pro.local>
2018-04-09 12:02:13 -0400

    now we can actually write a program to solve quadratic equations

---
+++ Quadratic.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Quadratic {
+
+    // This program solves quadratic equations
+    //
+    //                      Jawaun Brown
+
+    public static Scanner keyboard = new Scanner(System.in);
+
+    public static void main (String[] args) {
+
+	System.out.println ("This program solves quadratic equations of the");
+	System.out.println ("form a*(x^2) + b*x + c = 0.");
+	System.out.println ();
+	System.out.print ("Enter integers for a, b, and c: ");
